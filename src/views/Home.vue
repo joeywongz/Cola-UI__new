4ed0f79cbc104a543e252840873f6fc5da9452b9
wangchong <370318843@qq.com>
2021-03-16 22:25:38 +0800

    封装Topnav组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,16 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>Cola UI</h1>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-  .topnav {
-    border: 1px solid red;
-    display: flex;
-    padding: 16px;
-    > .logo {
-      max-width: 6em;
-      margin-right: auto;
-    }
-    > .menu {
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      > li {
-        margin: 0.1em
-      }
-    }
-  }
+<script lang="ts">
+  import Topnav from '../components/Topnav.vue'
 
-</style>
+  export default {
+    components: {Topnav}
+  }
+</script>
