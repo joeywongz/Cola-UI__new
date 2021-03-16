379e4a39585925f28abecc70d6385ef2ca6168fe
wangchong <370318843@qq.com>
2021-03-16 22:08:24 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="log"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Cola UI</h1>
-      <h2>一个好看的UI框架</h2>
-      <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
-      </p>
     </div>
   </div>
 </template>
@@ -18,5 +16,20 @@
 <style lang="scss" scoped>
   .topnav {
     border: 1px solid red;
-  }  
+    display: flex;
+    padding: 16px;
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li {
+        margin: 0.1em
+      }
+    }
+  }
+
 </style>
